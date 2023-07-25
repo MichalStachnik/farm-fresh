08e4e2b6d40bf3b2180bedb023575804a1c101f7
Michal Stachnik <michal@prl.one>
2023-07-25 10:00:17 -0400

    removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import FarmMap from '@/components/FarmMap';
+import { ViewportProvider } from '@/contexts/ViewportContext';
 import FarmMapWrapper from '@/components/FarmMapWrapper';
 import LocationButton from '@/components/LocationButton';
 import SearchBar from '@/components/SearchBar';
-import { ViewportProvider } from '@/contexts/ViewportContext';
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-8">farm fresh</div>
-      {/* TODO: add request form and header */}
       <ViewportProvider>
         <SearchBar />
         <LocationButton />
